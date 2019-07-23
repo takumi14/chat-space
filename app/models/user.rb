ec9eba89f2b889527d7dfd8d65b1a272fe7ff2d9
takumi14 <03serious@gmail.com>
2019-07-23 17:10:04 +0900

    group-create-function

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :messages
-  has_many :groups_users
-  has_many :groups, through: :groups_users
+  has_many :group_users
+  has_many :groups, through: :group_users
 end
