85ea16bb1988298f9b6c6ef4c68499337ef6965a
takumi14 <03serious@gmail.com>
2019-08-06 17:08:11 +0900

    ajax-message-function
    非同期通信の実装。

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,30 @@
 $(function(){
-  $('#new_message').on('submit', function(){
-    console.log(this)
+  
+  function buildHTML(message) {
+    var image = message.image? `${message.image}` : "";
+    var html = `<div class="message">
+                  <div class = "message__upper-info">
+                    <div class = "message__upper-info__user">
+                      ${message.user_name}
+                    </div>
+                    <div class = "message__upper-info__data">
+                      ${message.time}
+                    </div>
+                  </div>
+                  <div class = "message__text">
+                    ${message.content}
+                  </div>
+                  <div>
+                    <img class ="lower-message__image" src="${message.image}">
+                  </div>
+                </div>`
+
+    return html
+  }
+
+
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
     var formData = new FormData(this);
     var url = $(this).attr('action')
     $.ajax({
@@ -10,7 +34,19 @@ $(function(){
       dataType: 'json',
       processData: false,
       contentType: false
-
     })
+
+    .done(function(message) {
+      console.log(message)
+      var html = buildHTML(message);
+      $('.messages').append(html);
+      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight});
+      $('.form__submit').prop('disabled', false);
+      $("#new_message")[0].reset();
+    })
+    .fail(function() {
+      alert('error');
+      $('.form__submit').prop('disabled', false);
+    });
   })
-})
+})  
