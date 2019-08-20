eeb042f627b418e47c0fa3eb0340cc006c70b00c
takumi14 <03serious@gmail.com>
2019-08-20 17:07:46 +0900

    message.js
    自動更新のためj query発火

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,9 +1,14 @@
 class Api::MessagesController < ApplicationController
   def index
-  @group = Group.find(params[:group_id])
-    respond_to do |format|  
-      format.html 
-      format.json { @messages = @group.messages.includes(:user).where('id >?',params[:message_id]) }
+    @message = Message.new
+
+    @group = Group.find(params[:group_id])
+
+    @messages = @group.messages.includes(:user)
+
+    respond_to do |format|
+      format.html
+      format.json { @new_messages = @messages.where('id > ?', params[:id]) }
     end
   end
-end
+end 
