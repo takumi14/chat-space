ebce700bbf44ff69ff53beb04d3590b13159f85a
takumi14 <03serious@gmail.com>
2019-08-24 00:09:25 +0900

    非同期通信機能修正

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,3 +1,61 @@
+
+
+
+$(function(){
+  
+  function buildHTML(message) {
+    var image = message.image? `${message.image}` : "";
+    var html = `<div class="message">
+                  <div class = "message__upper-info">
+                    <div class = "message__upper-info__user">
+                      ${message.user_name}
+                    </div>
+                    <div class = "message__upper-info__data">
+                      ${message.time}
+                    </div>
+                  </div>
+                  <div class = "message__text">
+                    ${message.content}
+                  </div>
+                  <div>
+                    <img class ="lower-message__image" src="${message.image}">
+                  </div>
+                </div>`
+
+    return html
+  }
+
+
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+
+    .done(function(message) {
+      console.log(message)
+      var html = buildHTML(message);
+      $('.messages').append(html);
+      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight});
+      $('.form__submit').prop('disabled', false);
+      $("#new_message")[0].reset();
+    })
+    .fail(function() {
+      alert('error');
+      $('.form__submit').prop('disabled', false);
+    });
+  })
+})  
+
+
+
 $(function() {
   var buildHTML = function(message) {
     var content = message.content ? `${ message.content }` : "";
@@ -12,12 +70,15 @@ $(function() {
                     </div>
                   </div>
                   <div class = "message__text">
-                  ${content}
+                   ${content}
                   </div>
-                  ${img}
+                   ${img}
                 </div>`
     return html;
   };
+
+    
+
     var reloadMessages = function() {
 
       var last_message_id = $('.message:last').data('message-id');
